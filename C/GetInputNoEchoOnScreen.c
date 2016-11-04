df03770ed37b4c139f41b9b23283ddb7562ab7c4
xieee3 <prhsie@gmail.com>
2016-11-05 04:40:09 +0800

    Read Character without echo on screen

--- C/GetInputNoEchoOnScreen.c
+++ C/GetInputNoEchoOnScreen.c
@@ -1,21 +1,30 @@
+//
+// Platform : Windows
+//
+// Read characters without echo on the screen.
+//
 #include <stdio.h>
 #include <string.h>
 #include <conio.h>
 #include <windows.h>
 #define StringLength 6
 
-TCHAR _getch1() {
-  DWORD mode, cc;
-  HANDLE h = GetStdHandle( STD_INPUT_HANDLE );
-  if (h == NULL) {
+//***
+// Because getch() is not a standard function. getch is replaced by _getch.
+// A workaround function ReadCharWithoutEcho() has the same ability with getch.
+//***
+TCHAR ReadCharWithoutEcho() {
+    DWORD mode, cc;
+    HANDLE h = GetStdHandle( STD_INPUT_HANDLE );
+    if (h == NULL) {
         return 0; // console not found
-  }
-  GetConsoleMode( h, &mode );
-  SetConsoleMode( h, mode & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT) );
-  TCHAR c = 0;
-  ReadConsole( h, &c, 1, &cc, NULL );
-  SetConsoleMode( h, mode );
-  return c;
+    }
+    GetConsoleMode( h, &mode );
+    SetConsoleMode( h, mode & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT) );
+    TCHAR c = 0;
+    ReadConsole( h, &c, 1, &cc, NULL );
+    SetConsoleMode( h, mode );
+    return c;
 }
 
 int main()
@@ -23,7 +32,7 @@ int main()
     int Count;
     char Ptr[StringLength];
     Count = 0;
-    Ptr[Count] = _getch1();
+    Ptr[Count] = ReadCharWithoutEcho();
     while (Count < StringLength)
     {
         Ptr[Count] = getchar();
@@ -35,7 +44,6 @@ int main()
         if (Ptr[Count] == '\r' || Ptr[Count] == '\n')
             break;
     }
-
     Ptr[Count] = '\0';
     printf("\n%s %d", Ptr, strlen(Ptr));
     return 0;
